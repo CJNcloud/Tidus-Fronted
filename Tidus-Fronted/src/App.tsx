4af18baced12dd075d218f08b923103e2db8e626
yuan lu <2766508815@qq.com>
2025-07-14 14:50:10 +0800

    test

--- Tidus-Fronted/src/App.tsx
+++ Tidus-Fronted/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <HomePage></HomePage>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
